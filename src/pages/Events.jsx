d846b99177903103e8bd877d0380cdc110645939
Roshan Gupta <roshan.gupta@occamsadvisory.com>
2025-09-22 16:39:52 +0530

    press media and events read more same alignment

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -20,25 +20,27 @@ const Events = () => {
 
             <div className="lg:px-[6rem] sm:px-7 px-5 pt-12">
                 <div className='container mx-auto'>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 text-left">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-8 gap-y-[60px] h-full text-left">
                         {events?.map((event, index) => (
-                            <div key={index}>
-                                <div className="rounded-[30px] overflow-hidden">
-                                    <img src={event?.image} alt={event?.title} />
+                            <div key={index} className='flex flex-col h-full justify-between'>
+                                <div>
+                                    <div className="rounded-[30px] overflow-hidden">
+                                        <img src={event?.image} alt={event?.title} />
+                                    </div>
+
+                                    <h2 className="font-semibold text-[#3c0a6d] text-[21px] pt-[30px] inter_medium
+                                    line-clamp-3"
+                                    >
+                                        {event?.title}
+                                    </h2>
+
+                                    <p className="text-[17.92px] text-black inter mt-2.5 line-clamp-5">
+                                        {event?.description}
+                                    </p>
                                 </div>
 
-                                <h2 className="font-semibold text-[#3c0a6d] text-[21px] pt-[30px] inter_medium
-                                    line-clamp-3"
-                                >
-                                    {event?.title}
-                                </h2>
-
-                                <p className="text-[17.92px] text-black inter mt-2.5 line-clamp-5">
-                                    {event?.description}
-                                </p>
-
-                                <Link to={`/events/${slugify(event?.title)}`} target="_blank" className='mt-6 text-[#3c0a6d] 
-                                    flex gap-1 inter_semibold hover:text-[#0a58ca] items-center inter'
+                                <Link to={`/events/${slugify(event?.title)}`} target="_blank" className='mt-4 gap-1
+                                    flex text-[#3c0a6d] inter_semibold hover:text-[#0a58ca] items-center inter'
                                 >
                                     <p className='sm:text-lg text-sm font-normal'>Read More</p>
                                     <img src={'/icons/cta_arrow.svg'} alt='cta_arrow' className="mt-0.5" />
--- src/pages/PressMedia.jsx
+++ src/pages/PressMedia.jsx
@@ -16,25 +16,27 @@ const PressMedia = () => {
 
             <div className="lg:px-[6rem] sm:px-7 px-5 pt-12">
                 <div className='container mx-auto'>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 text-left">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-8 gap-y-[60px] text-left h-full">
                         {press_media?.map((media, index) => (
-                            <div key={index}>
-                                <div className="rounded-[30px] overflow-hidden">
-                                    <img src={media?.image} alt={media?.title} />
+                            <div key={index} className='flex flex-col h-full justify-between'>
+                                <div>
+                                    <div className="rounded-[30px] overflow-hidden">
+                                        <img src={media?.image} alt={media?.title} />
+                                    </div>
+
+                                    <h2 className="font-semibold text-[#3c0a6d] text-[21px] pt-[30px] inter_medium
+                                    line-clamp-3"
+                                    >
+                                        {media?.title}
+                                    </h2>
+
+                                    <p className="text-[17.92px] text-black inter mt-2.5">
+                                        {media?.description}
+                                    </p>
                                 </div>
 
-                                <h2 className="font-semibold text-[#3c0a6d] text-[21px] pt-[30px] inter_medium
-                                    line-clamp-3"
-                                >
-                                    {media?.title}
-                                </h2>
-
-                                <p className="text-[17.92px] text-black inter mt-2.5">
-                                    {media?.description}
-                                </p>
-
-                                <Link to={`/press_media/${media?.id}`} target="_blank" className='mt-6
-                                    inter_semibold hover:text-[#0a58ca] items-center inter text-[#3c0a6d] flex gap-1'
+                                <Link to={`/press_media/${media?.id}`} target="_blank" className='mt-4 gap-1
+                                    inter_semibold hover:text-[#0a58ca] items-center inter text-[#3c0a6d] flex'
                                 >
                                     <p className='sm:text-lg text-sm font-normal'>Read More</p>
                                     <img src={'/icons/cta_arrow.svg'} alt='cta_arrow' className="mt-0.5" />
